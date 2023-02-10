69c75366a23d8756ac6761c6ec2fa8e82ab8f49b
Luan Vinicius <nalucode@gmail.com>
2023-02-10 16:52:19 -0300

    Refatoração e implementação de funcionalidades.
    Funcionalidade de alterar tarefas já existentes;
    Melhorias dos estilos das páginas;
    Remoção de arquivos em desuso;

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Tasks from "./components/index.jsx";
 
 function App() {
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -6,61 +6,91 @@ function Tasks() {
   const [loading, setLoading] = React.useState(false);
 
   const fetchData = async () => {
-    const rawData = await fetch("http://localhost:3000/tasks", {
-      method: "GET",
-    });
-    const parsedData = await rawData.json();
+    try {
+      const rawData = await fetch("http://localhost:3000/tasks", {
+        method: "GET",
+      });
+      const parsedData = await rawData.json();
 
-    setTasks(parsedData);
+      setTasks(parsedData);
+    } catch (err) {
+      alert("Erro ao carregar as tasks");
+      console.error(err);
+    }
   };
 
+  // Carrega as tarefas iniciais.
   React.useEffect(() => {
     fetchData();
   }, []);
 
   const handleSubmitClick = async () => {
+    const bodyData = JSON.stringify({ task: inputValue });
+
     setLoading(true);
 
-    const bodyData = JSON.stringify({ task: inputValue})
-
     try {
-      await fetch("http://localhost:3000/tasks", {
+      const rawData = await fetch("http://localhost:3000/tasks", {
         method: "POST",
         body: bodyData,
-        headers: { 'Content-Type': 'application/json' }
-      })
+        headers: { "Content-Type": "application/json" },
+      });
 
-      setTasks([...tasks, {id: 20, task: inputValue} ])
-    } catch(err) {
-      alert('Erro na requisiçã.')
+      const taskData = await rawData.json();
+
+      setTasks([...tasks, taskData]);
+    } catch (err) {
+      alert("Erro ao criar nova task.");
+      console.error(err);
     }
 
     setLoading(false);
   };
 
+  const handleUpdateTask = async (taskId) => {
+    const taskBody = prompt("Qual o novo nome da tarefa?");
+    const bodyData = JSON.stringify({ task: taskBody });
+
+    await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: bodyData,
+    });
+
+    setTasks(tasks.map((t) => (t.id == taskId ? { ...t, task: taskBody } : t)));
+  };
+
   return (
-    <>
+    <div className="tasks">
       <ul>
         {tasks.length
-          ? tasks.map((t, i) => {
+          ? tasks.map((t) => {
               return (
-                <li key={i}>
-                  {t.id}-{t.task}
+                <li key={t.id}>
+                  <span>
+                    {t.id} - {t.task}
+                  </span>
+                  <button onClick={() => handleUpdateTask(t.id)}>
+                    Alterar
+                  </button>
                 </li>
               );
             })
-          : "No task"}
+          : "Nenhuma task encontrada."}
       </ul>
-
-      <input
-        type="text"
-        onChange={(e) => setInputValue(e.target.value)}
-        value={inputValue}
-      />
-      <button disabled={loading} onClick={handleSubmitClick}>
-        Criar task
-      </button>
-    </>
+      
+      <div className="add-task-form-group">
+        <input
+          type="text"
+          placeholder="Aprovar esse PR"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button disabled={loading} onClick={handleSubmitClick}>
+          Criar task
+        </button>
+      </div>
+    </div>
   );
 }
 
